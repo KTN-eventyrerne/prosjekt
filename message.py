843c3dc6022dfa222bb4a2c488003391a59d0717
Kjetil Kjeka <kjetibk@stud.ntnu.no>
2014-03-15 15:41:16 +0100

    Message is now working perfectly

---
+++ message.py
@@ -0,0 +1,44 @@
+import json
+
+class Message(object):
+
+    def __init__(self):
+        self.request = ''
+        self.message = ''
+        self.response = ''
+        self.error = ''
+        self.username = ''
+        
+    def parse(self, message):
+        self.request = message[0].get('request', '')
+        self.message = message[0].get('message', '')
+        self.response = message[0].get('response', '')
+        self.error = message[0].get('error', '')
+        self.username = message[0].get('username', '')
+
+        self.validate()
+        
+    def serialize(self):
+        self.validate()
+
+        jmsg = {}
+        if(self.request != ''):
+            jmsg.update({'request': self.request})
+        if(self.message != ''):
+            jmsg.update({'message': self.message})
+        if(self.response != ''):
+            jmsg.update({'response': self.response})
+        if(self.error != ''):
+            jmsg.update({'error': self.error})
+        if(self.username != ''):
+            jmsg.update({'username': self.username})
+ 
+        return json.dumps([jmsg])
+
+    def validate(self):
+        if(self.request == '' and self.response == ''):
+            raise Exception('No request or response field defined')
+        if(self.request != '' and self.response != ''):
+            raise Exception('Both request and response field defined')
+        if(self.message == '' and self.username == '' and self.error == ''):
+            raise Exception('No message, username nor error. No payload in message')
